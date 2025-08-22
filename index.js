b2809bdfcd9aeadee8c03cea50f1ab5035f74b7e
Eugene S <deploy@jekil1.fvds.ru>
2025-08-23 01:17:42 +0300

    Change default port to 3005

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import connectDB from './config/db.js';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
 async function startServer() {
   try {
